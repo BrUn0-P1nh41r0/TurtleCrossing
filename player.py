6cc9b30cc8050109befd54d3a9935fd779c4eee9
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-10 12:10:39 +0000

    Completed turtle crossing game

--- player.py
+++ player.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+        self.move_speed = 0.1
+
+    def move_player(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def next_level(self):
+        self.goto(STARTING_POSITION)
+        self.move_speed *= 0.9
+
